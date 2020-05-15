7a77d7dceb400ef640cc2eb7ea1cf6bcfc1c1635
Felipe Vieira <flpvieira86@gmail.com>
2020-05-15 15:43:04 -0300

    exercicio 4.4

---
+++ Bloco4/funcoes.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+	return a + b;
+}
+function sub(a, b) {
+	return a - b;
+}
+function mult(a, b) {
+	return a * b;
+}
+function div(a, b) {
+	return a / b;
+}
+function mod(a,b) {
+  return a % b;
+}
+
+console.log("SOMA = " + sum(2,3));
+console.log("SUBTRAÇÃO = " + sub(2,3));
+console.log("MULTIPLICAÇÃO = " + mult(2,3));
+console.log("DIVISÃO = " + div(2,3));
+console.log("MODULO = " + mod(2,3));
