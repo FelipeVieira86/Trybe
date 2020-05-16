a83477c76b08cb6e0c04002ba8a806e459575a1e
Felipe Vieira <flpvieira86@gmail.com>
2020-05-15 21:26:40 -0300

    renamed

--- Bloco4/sumallarraynumbers.js
+++
@@ -1,8 +0,0 @@
-//você deve somar todos os valores contidos no array e imprimir o resultado
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let total = 0;
-for (i = 0; i < numbers.length; i+=1) {
-  total += numbers[i];
-}
-console.log(total)
