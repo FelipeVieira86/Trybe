d47195ae60451d7c14ccf394fafb7ddfe42e71e3
Felipe Vieira <flpvieira86@gmail.com>
2020-07-14 01:53:28 -0300

    bonus botao desativado

--- Bloco12/exercicio12-1/src/Button.js
+++ Bloco12/exercicio12-1/src/Button.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 class Button extends Component {
   render() {
-    return (<button onClick={this.props.onClick}>{this.props.label}</button>)
+    return (
+      <button disabled={this.props.disabled} onClick={this.props.onClick}>
+        {this.props.label}
+      </button>
+    );
   }
 }
 
--- Bloco12/exercicio12-1/src/pokedex.js
+++ Bloco12/exercicio12-1/src/pokedex.js
@@ -15,15 +15,15 @@ class pokedex extends React.Component {
       : this.setState({ index: 0 });
   }
 
-  filterType() {
+  filterType(type) {
     return pokemonList.filter(pokemon => {
-      if (this.state.pokemonType === 'All') return true;
-      return pokemon.type === this.state.pokemonType;
+      if (type === 'All') return true;
+      return pokemon.type === type;
     });
   }
 
   setType(type) {
-    this.setState({ pokemonType: type , index: 0});
+    this.setState({ pokemonType: type, index: 0 });
   }
 
   pokemonTypesArray = () =>
@@ -32,13 +32,19 @@ class pokedex extends React.Component {
       [],
     );
 
+  
+
   render() {
     const pokemonsList = this.filterType(this.state.pokemonType);
     const pokemonTypes = this.pokemonTypesArray();
     return (
       <div>
         <Pokemons pokemon={pokemonsList[this.state.index]} />
-        <Button onClick={() => this.nextPokemon(pokemonsList.length)} label="Proximo Pokemon" />
+        <Button
+          disabled={this.filterType(this.state.pokemonType).length <= 1}
+          onClick={() => this.nextPokemon(pokemonsList.length)}
+          label="Proximo Pokemon"
+        />
         <Button onClick={() => this.setType('All')} label="All" />
         {pokemonTypes.map(type => (
           <Button onClick={() => this.setType(type)} label={type} />
