1d1daa9c26257f71a53e225952de1151e9e702fa
Felipe Vieira <flpvieira86@gmail.com>
2020-07-14 00:59:04 -0300

    etapa2

--- Bloco12/exercicio12-1/src/pokedex.js
+++ Bloco12/exercicio12-1/src/pokedex.js
@@ -6,22 +6,32 @@ import Button from './Button';
 class pokedex extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { index: 0 };
+    this.state = { index: 0 , pokemonType: 'Psychic'};
   }
 
   nextPokemon(arrayLength) {
-    console.log(this.state.index);
-    arrayLength-1 > this.state.index
+    arrayLength - 1 > this.state.index
       ? this.setState({ index: this.state.index + 1 })
       : this.setState({ index: 0 });
   }
 
+  filterType() {
+    return pokemonList.filter(pokemon => pokemon.type === this.state.pokemonType)
+  }
+
+  setType(type) {
+    this.setState({pokemonType: type})
+  }
+
   render() {
-    const pokemon = pokemonList.filter(pokemon => pokemon);
+    const pokemonsList = this.filterType(this.state.pokemonType)
+    const pokemon = pokemonsList[this.state.index];
     return (
       <div>
-        <Pokemons pokemon={pokemon[this.state.index]} />
-        <Button onClick={() => this.nextPokemon(pokemonList.length)} label="Proximo Pokemon" />
+        <Pokemons pokemon={pokemon} />
+        <Button onClick={() => this.nextPokemon(pokemonsList.length)} label="Proximo Pokemon" />
+        <Button onClick={() => this.setType('Fire')} label="Fire" />
+        <Button onClick={() => this.setType('Psychic')} label="Psychic" />
       </div>
     );
   }
