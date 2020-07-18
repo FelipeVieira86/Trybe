d30e5f495beff8bff9e5d74407e73fa476577efa
Felipe Vieira <flpvieira86@gmail.com>
2020-07-18 20:46:28 -0300

    exercicio 13-1

---
+++ Bloco13/exercicio13-1-2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dropdown from './Dropdown'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dropdown>
+        <div>Titulo do Drop</div>
+      </Dropdown>
+    </div>
+  );
+}
+
+export default App;
