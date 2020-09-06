9ef665811a05115e2c2b496e904064a291d7fa82
Felipe Vieira <flpvieira86@gmail.com>
2020-09-06 14:29:00 -0300

    Updates

---
+++ precache-manifest.44219c6e56fbd40750de438353fb9a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c3fc39563a8c48da83b73bbc2f1d86",
+    "url": "/MovieLib/index.html"
+  },
+  {
+    "revision": "4321f3c617a482ab975e",
+    "url": "/MovieLib/static/css/main.2260e4d5.chunk.css"
+  },
+  {
+    "revision": "d2f538efcfce92d1f938",
+    "url": "/MovieLib/static/js/2.2e5f002d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieLib/static/js/2.2e5f002d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4321f3c617a482ab975e",
+    "url": "/MovieLib/static/js/main.88774e55.chunk.js"
+  },
+  {
+    "revision": "35722d07aa6910042f6f",
+    "url": "/MovieLib/static/js/runtime-main.3ec3c471.js"
+  }
+]);
